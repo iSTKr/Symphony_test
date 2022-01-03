557bd225bb464c2cf1d82e291dcd05e13963bf0f
iSTKr <didenko.anton.v@gmail.com>
2022-01-03 23:48:13 +0000

    +

---
+++ infra/init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Updating"
+apt update
+apt upgrade
+
+echo "Installing GIT"
+apt install -y git
+git --version
+
+echo "Configuring GIT"
+mkdir git && cd git
+git init
+git remote add origin https://github.com/iSTKr/Symphony_test.git
+git pull origin main
+
+echo "Installing Docker"
+apt install -y docker.io
+docker --version
+
+echo "Configuring Docker"
+cd /git/infra
+docker build -t apache .
+docker run -v /git/main/CV:/git -d -p 80:80 apache
---
+++ infra/main.tf
@@ -0,0 +1,85 @@
+variable "awsprops" {
+    type = map
+    default = {
+    region = "eu-central-1"
+    vpc = "vpc-0229b5f1ffd1920b3"
+    ami = "ami-0d527b8c289b4af7f"
+    itype = "t2.micro"
+    subnet = "subnet-086e3e114afd0d070"
+    publicip = true
+    keyname = "Symphony"
+    secgroupname = "Symphony"
+  }
+}
+
+provider "aws" {
+  region = lookup(var.awsprops, "region")
+  shared_credentials_file = "/home/stk/.aws/credentials"
+  
+}
+
+resource "aws_security_group" "symphony" {
+  name = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id = lookup(var.awsprops, "vpc")
+
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_instance" "symphony_instance" {
+  ami = lookup(var.awsprops, "ami")
+  instance_type = lookup(var.awsprops, "itype")
+  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name = lookup(var.awsprops, "keyname")
+
+
+  vpc_security_group_ids = [
+    aws_security_group.symphony.id
+  ]
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size = 8 
+    volume_type = "gp2"
+  }
+  tags = {
+    Name ="SERVER"
+    Environment = "DEV"
+    OS = "UBUNTU"
+    Managed = "SYMPHONY"
+  }
+
+  depends_on = [ aws_security_group.symphony ]
+  user_data = "${file("init.sh")}"
+}
+
+output "ec2instance" {
+  value = aws_instance.symphony_instance.public_ip
+}
