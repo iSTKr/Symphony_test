0f6569ceb8ed8ddf6973ff9219c64566649ae7f0
iSTKr <didenko.anton.v@gmail.com>
2022-01-04 21:39:22 +0200

    +

--- infra/init.sh
+++ infra/init.sh
@@ -23,14 +23,3 @@ cd /git/infra
 docker build -t apache .
 docker run --name apache2 -d -p 80:80 apache
 
-#echo "Downloading actions-runner"
-#cd /
-#mkdir actions-runner && cd actions-runner
-#curl -o actions-runner-linux-x64-2.285.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.285.1/actions-runner-linux-x64-2.285.1.tar.gz
-#echo "5fd98e1009ed13783d17cc73f13ea9a55f21b45ced915ed610d00668b165d3b2  actions-runner-linux-x64-2.285.1.tar.gz" | shasum -a 256 -c
-#tar xzf ./actions-runner-linux-x64-2.285.1.tar.gz
-
-#echo "Configuring actions-runner"
-#./config.sh --url https://github.com/iSTKr/Symphony_test --token AXB5DQ772M37V5I5SYHBAP3B2RI2K
-#./run.sh
-
