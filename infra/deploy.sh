6f958941b74769082e48bfff2f47c856a6c3e434
iSTKr <didenko.anton.v@gmail.com>
2022-01-05 11:51:59 +0200

    +

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo terraform apply
-sudo terraform output ec2instance_dns > ec2_dns_name && sudo cp ec2_dns_name home/stk/symphony_test/git/main/infra/ec2_dns_name
+sudo terraform output ec2instance_dns > ec2_dns_name
+#sudo cp ec2_dns_name home/stk/symphony_test/git/main/infra/ec2_dns_name
 read -t 70 -p "Please, wait a minute before pushing...
 "
 cd home/stk/symphony_test/git/main && ./git_push
